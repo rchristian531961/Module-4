63cd51cde24f753e9d8e56922e5a070246fafb0a
Ray Christian <rchristian531961@yahoo.com>
2023-05-07 18:48:55 -0500

    Completed Excercise 1

--- Exercise-1-js.js
+++ Exercise-1-js.js
@@ -1,11 +1,54 @@
 function addCard() {
-
     const template = document.getElementById("card-template").content.cloneNode(true);
-    
     template.querySelector('.card-title').innerText = 'My Card Title';
     template.querySelector('.card-text').innerText = 'lorem ipsum ble bla';
     document.querySelector('#card-list').appendChild(template);
+}
+
+//  if ('content' in document.createElement('template')) {
+//       addCard();
+// }
+
+const data=[{name:'bob',age:23}, {name:'alice',age:39}];
+
+function addCardData(){
+    //Can't just have const template (one variable)
+    //It needs to reclone the object for every element
+    for(i=0;i<data.length;i++){
+        const template=document.getElementById("card-template").content.cloneNode(true);
+        template.querySelector(".card-title").innerText=data[i].name;
+        template.querySelector(".card-text").innerText=data[i].age;
+        document.querySelector('#card-list').appendChild(template);
     }
-    if ('content' in document.createElement('template')) {
-    addCard();
+}
+
+const artist={
+    name:"Van Gogh",
+    portfolio:[{title:"portrait",
+    url:"https://collectionapi.metmuseum.org/api/collection/v1/iiif/436532/1671316/main-image"},
+    {title:"sky",url:"https://mymodernmet.com/wp/wpcontent/uploads/2020/11/White-house-night-van-gohworldwide-2.jpg"},
+    ]
+}
+
+function portfolioList(){
+    const template=document.getElementById("card-template-2").content.cloneNode(true);
+    template.querySelector(".card-name").innerText=artist.name;
+    template.querySelector(".card-title").innerText=artist.portfolio[0].title;
+    var x=document.createElement("img");
+    x.setAttribute("src",artist.portfolio[0].url);
+    template.querySelector(".card-photo").appendChild(x);
+    document.querySelector("#portfolio-list").appendChild(template);
+}
+
+function portfolioList_2(){
+    for (i=0;i<artist.portfolio.length;i++){
+        const template=document.getElementById("card-template-2").content.cloneNode(true); //copy of card-template tag
+        template.querySelector(".card-name").innerText=artist.name;
+        template.querySelector(".card-title").innerText=artist.portfolio[i].title;
+        var cur_img=document.createElement("img");
+        cur_img.setAttribute("src",artist.portfolio[i].url);
+        template.querySelector(".card-photo").appendChild(cur_img);
+        document.querySelector("#portfolio-list").appendChild(template);
     }
+    
+}
--- practice-js.js
+++ practice-js.js
@@ -1,17 +1,23 @@
-var myArr = ["Audi", "BMW", "Ford", "Honda", "Jaguar", "Nissan"];
 
-function showContent() {
-  var temp, item, a, i;
-  temp = document.getElementsByTagName("template")[0];
-  //get the div element from the template:
-  item = temp.content.querySelector("div");
-  //for each item in the array:
-  for (i = 0; i < myArr.length; i++) {
-    //Create a new node, based on the template:
-    a = document.importNode(item, true);
-    //Add data from the array:
-    a.textContent += myArr[i];
-    //append the new node wherever you like:
+var myArr=["Audi","BMW", "Ford","Honda","Jaguar","Nissan"];
+
+function showContent(){
+  var temp,item,a,i;    
+  temp=document.getElementsByTagName("template")[0];
+  item=temp.content.querySelector("div");
+  for (i=0;i<myArr.length;i++){
+    a=document.importNode(item,true);
+    a.textContent+=myArr[i];
     document.body.appendChild(a);
   }
 }
+
+//Clone the template tag first
+const main=document.getElementById('wrap');//reference to html wrap object
+const template=document.getElementById('card-template');//reference to html card-template
+const content=template.content.cloneNode(true); //contains all html of template
+main.appendChild(content);//added to the wrap element from template
+
+const content2=template.content.cloneNode(true);
+main.append(content2);
+
